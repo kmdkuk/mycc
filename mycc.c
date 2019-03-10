a9fb0012031e09490b9a3546a70f30df96dfd70f
kouki <kouworld0123@gmail.com>
2019-03-10 20:25:47 +0900

    :sparkles: add gen関数

--- mycc.c
+++ mycc.c
@@ -156,6 +156,39 @@ void tokenize(char *p)
   tokens[i].input = p;
 }
 
+void gen(Node *node)
+{
+  if (node->ty == ND_NUM)
+  {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->ty)
+  {
+  case '+':
+    printf("  add rax, rdi\n");
+    break;
+  case '-':
+    printf("  sub rax, rdi\n");
+    break;
+  case '*':
+    printf("  mul rdi\n");
+    break;
+  case '/':
+    printf("  mov rdx, 0\n");
+    printf("  div rdi\n");
+  }
+
+  printf(" push rax\n");
+}
+
 int main(int argc, char **argv)
 {
   if (argc != 2)
